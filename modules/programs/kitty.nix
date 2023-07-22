67deec2c6131d02d4b7cd5f900780a58266f41d0
spector700 <72362399+spector700@users.noreply.github.com>
2023-07-21 22:30:33 -0500

    changed kitty

--- modules/programs/kitty.nix
+++ modules/programs/kitty.nix
@@ -4,14 +4,18 @@
   programs = {
     kitty = {
       enable = true;
-	font = {                          # Font - Laptop has size manually changed at home.nix
-	  name = "JetBrainsMono Nerd Font";
-	  #size = 8;
-        };
-      theme = "Tokyo Night";
-      settings = {
-	confirm_os_window_close = 0;	
+      font = {
+        # Font - Laptop has size manually changed at home.nix
+        name = "JetBrainsMono Nerd Font";
+        #size = 8;
       };
-     };
+      shellIntegration.enableZshIntegration = true;
+
+      theme = "Catppuccin-Mocha";
+      settings = {
+        confirm_os_window_close = 0;
+        enable_audio_bell = false;
+      };
     };
+  };
 }
