4087a3b88715bb970ef03d6b7e92576111220443
spector700 <72362399+spector700@users.noreply.github.com>
2024-02-10 00:41:28 -0600

    changed lazyvim to the Akari nixvim

--- home-modules/editors/neovim/default.nix
+++ home-modules/editors/neovim/default.nix
@@ -1,41 +1,10 @@
 # Neovim
 #
-{ pkgs, config, location, ... }: {
-  home.file.".config/nvim".source = config.lib.file.mkOutOfStoreSymlink
-    "${location}/home-modules/editors/neovim/nvim";
+{ inputs, pkgs, ... }: {
 
-  programs.neovim = {
-    enable = true;
-    plugins = with pkgs.vimPlugins; [ nvim-treesitter.withAllGrammars ];
-
-    vimAlias = true;
-    viAlias = true;
-    vimdiffAlias = true;
-
-    withNodeJs = true;
-    # withPython3 = true;
-  };
   home.packages = with pkgs; [
-    # LSP
-    nodePackages_latest.typescript-language-server
-    nodePackages_latest."@tailwindcss/language-server"
-    nodePackages_latest.bash-language-server
-    nodePackages_latest.pyright
-    sumneko-lua-language-server
-    nixd
-    ruff-lsp
-
-    # Format
-    nixpkgs-fmt
-    stylua
-    beautysh
-    nodePackages.prettier
-    black
-
-    gcc
-    # cargo
-    ripgrep
-    fd
+    inputs.Akari.packages.${system}.default
     lazygit
+    repgrep
   ];
 }
