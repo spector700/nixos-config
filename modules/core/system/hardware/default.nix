2cfc40a253faade75b933cb9bd736987fb5eddd5
spector700 <72362399+spector700@users.noreply.github.com>
2024-04-06 16:26:29 -0500

    feat: added roles template, removed colors lib

--- modules/core/system/hardware/default.nix
+++ modules/core/system/hardware/default.nix
@@ -10,20 +10,6 @@ in
   ];
 
   options.modules.hardware = {
-    type = mkOption {
-      type = types.enum [ "laptop" "desktop" "server" "hybrid" "lite" "vm" ];
-      default = "";
-      description = ''
-        The type/purpose of the device that will be used within the rest of the configuration.
-          - laptop: portable devices with batter optimizations
-          - desktop: stationary devices configured for maximum performance
-          - server: server and infrastructure
-          - hybrid: provide both desktop and server functionality
-          - lite: a lite device, such as a raspberry pi
-          - vm: a virtual machine
-      '';
-    };
-
     # the type of cpu your system has - vm and regular cpus currently do not differ
     cpu = {
       type = mkOption {
