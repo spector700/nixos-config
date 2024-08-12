af21c66774575ae83a55ae9add2ff40c8c56f551
spector700 <72362399+spector700@users.noreply.github.com>
2024-08-12 14:22:52 -0500

    feat: changed xdg portals

--- modules/nixos/display/wayland.nix
+++ modules/nixos/display/wayland.nix
@@ -35,14 +35,17 @@ in
 
       xdg.portal = {
         enable = true;
-        config = {
-          common.default = [
-            "hyprland"
-            "gtk"
-          ];
+        xdgOpenUsePortal = true;
+        config.common = {
+          "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
+          "org.freedesktop.impl.portal.ScreenCast" = [ "hyprland" ];
+          "org.freedesktop.impl.portal.Screenshot" = [ "hyprland" ];
+          "org.freedesktop.portal.FileChooser" = [ "xdg-desktop-portal-gtk" ];
         };
-
-        extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
+        extraPortals = [
+          pkgs.xdg-desktop-portal-hyprland
+          pkgs.xdg-desktop-portal-gtk
+        ];
       };
 
       # allow wayland lockers to unlock the screen
