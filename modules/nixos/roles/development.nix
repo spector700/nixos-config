57fceb6f5430a1639b8dc3d285b6779490aaaea9
spector700 <72362399+spector700@users.noreply.github.com>
2024-11-02 15:33:40 -0500

    feat: add esp32 development

--- modules/nixos/roles/development.nix
+++ modules/nixos/roles/development.nix
@@ -1,9 +1,27 @@
-{ lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 let
-  inherit (lib) mkEnableOption;
+  inherit (lib) mkIf mkEnableOption;
+  cfg = config.modules.roles.development;
 in
 {
   options.modules.roles.development = {
     enable = mkEnableOption "Enable the Development role";
+  };
+
+  config = mkIf cfg.enable {
+    # for platformio esp32 development
+    # nix-ld is needed for the platformio vscode extension to be able to run binaries
+    programs.nix-ld.enable = true;
+
+    services.udev.packages = with pkgs; [
+      platformio
+      openocd
+    ];
+
   };
 }
