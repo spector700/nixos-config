b7b3d84e564faf741df2f3f6580960aa872b0e32
spector700 <72362399+spector700@users.noreply.github.com>
2025-10-20 16:27:54 -0500

    fix: add hyprland game window rules

--- modules/home/spector/desktop/hyprland/config.nix
+++ modules/home/spector/desktop/hyprland/config.nix
@@ -49,46 +49,57 @@ in
       "noanim, gtk4-layer-shell"
     ];
 
-    windowrulev2 = [
-      # make Firefox/Zen PiP window floating and sticky
-      "float, title:^(Picture-in-Picture)$"
-      "pin, title:^(Picture-in-Picture)$"
+    windowrulev2 =
+      let
+        games = "^(steam_app_.*|lutris_game_class|ffxiv|minigalaxy|playnite_game_class|gamescope|chiaki|.*[Ww]ine.*)$";
+      in
+      [
+        # make Firefox/Zen PiP window floating and sticky
+        "float, title:^(Picture-in-Picture)$"
+        "pin, title:^(Picture-in-Picture)$"
 
-      # Force Screen tearing
-      # "immediate, class:^(steam_app_[0-9]*)$"
-      # "immediate, initialclass:^(steam_app_)(.*)$"
-      "immediate, class:ffxiv"
+        # Force Screen tearing
+        "immediate, class:${games}"
 
-      # fix fullscreen flashing
-      # "suppressevent fullscreen, class:^(steam_app_[0-9]*)$"
-      # "suppressevent fullscreen, class:ffxiv"
+        # fix fullscreen flashing
+        # "suppressevent fullscreen, class:^(steam_app_[0-9]*)$"
+        # "suppressevent fullscreen, class:ffxiv"
 
-      # Application in workspaces
-      "workspace 8 silent, title:^(Steam)|(Lutris)$"
-      "workspace 4 silent, title:^(.*((d|D)isc|ArmC|WebC)ord.*|vesktop)$"
-      "workspace special:spotify silent, title:^(Spotify.*)$"
+        #   Game Launchers
+        "workspace 8 silent, title:^(Steam)$"
+        "workspace 8, title:^(Lutris)$"
 
-      # idle inhibit while watching videos
-      "idleinhibit focus, class:^(mpv|.+exe|celluloid)$"
-      "idleinhibit focus, class:^(firefox|zen)$, title:^(.*YouTube.*)$"
-      "idleinhibit fullscreen, class:^(firefox|zen)$"
+        #   Games no animation, no blur, no shadow
+        "workspace 8 class:${games}"
+        "noanim, class:${games}"
+        "noblur, class:${games}"
+        "noshadow, class:${games}"
 
-      "dimaround, class:^(gcr-prompter)$"
-      "dimaround, class:^(xdg-desktop-portal-gtk)$"
-      "dimaround, class:^(polkit-gnome-authentication-agent-1)$"
+        "workspace 4 silent, title:^(.*((d|D)isc|ArmC|WebC)ord.*|vesktop)$"
+        "workspace special:spotify silent, title:^(Spotify.*)$"
+        "workspace special:spotify, title:^(signal)$"
 
-      # Fix xwayland apps
-      # "rounding 0, xwayland:1"
-      # "center, title:^(Confirm Exit|Open Project|win424|win201|splash)$"
-      # "size 640 400, title:^(splash)$"
+        # Fix steam menus
+        # "stayfocused, title:^()$,class:^(steam)$"
+        # "minsize 1 1, title:^()$,class:^(steam)$"
 
-      # Fix steam menus
-      "stayfocused, title:^()$,class:^(steam)$"
-      "minsize 1 1, title:^()$,class:^(steam)$"
+        # idle inhibit while watching videos
+        "idleinhibit focus, class:^(mpv|.+exe|celluloid)$"
+        "idleinhibit focus, class:^(firefox|zen)$, title:^(.*YouTube.*)$"
+        "idleinhibit fullscreen, class:^(firefox|zen)$"
 
-      # Opacity
-      "opacity 0.94 0.94,class:^(kitty|thunar|code(.*))$"
-    ];
+        "dimaround, class:^(gcr-prompter)$"
+        "dimaround, class:^(xdg-desktop-portal-gtk)$"
+        "dimaround, class:^(polkit-gnome-authentication-agent-1)$"
+
+        # Fix xwayland apps
+        # "rounding 0, xwayland:1"
+        # "center, title:^(Confirm Exit|Open Project|win424|win201|splash)$"
+        # "size 640 400, title:^(splash)$"
+
+        # Opacity
+        "opacity 0.94 0.94,class:^(kitty|thunar|code(.*))$"
+      ];
 
     general = {
       gaps_in = 5;
@@ -168,6 +179,8 @@ in
       disable_autoreload = true; # autoreload is unnecessary on nixos, because the config is readonly anyway
       disable_hyprland_logo = true;
       focus_on_activate = true; # Whether Hyprland should focus an app that requests to be focused (an activate request)
+      exit_window_retains_fullscreen = true; # if a fullscreen window is closed, the next window will be fullscreened if this is true
+      new_window_takes_over_fullscreen = 1;
 
       enable_swallow = true; # hide windows that spawn other windows
       swallow_regex = "kitty|thunar|wezterm"; # windows for which swallow is applied
