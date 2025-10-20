ba3f842f7e1f0493e1b463d06808c1c5c875074e
spector700 <72362399+spector700@users.noreply.github.com>
2025-10-20 16:28:25 -0500

    fix: add gpu reset rule

--- modules/nixos/display/graphical.nix
+++ modules/nixos/display/graphical.nix
@@ -22,6 +22,11 @@ in
         enable = true;
         enable32Bit = lib'.isx86Linux pkgs; # if we're on x86 linux, we can support 32 bit
       };
+
+      # Taken from Bazzite at https://github.com/ublue-os/bazzite/blob/17c869dc70eede3f7066a8ad9ed07f46798fa9b3/system_files/deck/shared/usr/lib/udev/rules.d/80-gpu-reset.rules
+      services.udev.extraRules = ''
+        ACTION=="change", ENV{DEVNAME}=="/dev/dri/card0", ENV{RESET}=="1", ENV{PID}!="0", RUN+="${pkgs.coreutils}/bin/kill -9 %E{PID}"
+      '';
     })
 
     (mkIf isWayland {
