564e0d458e7cd2a0af538ca511fd4a0b92224cb4
spector700 <72362399+spector700@users.noreply.github.com>
2026-03-09 12:42:34 -0500

    feat(nixcord): update config

--- modules/home/spector/programs/nixcord.nix
+++ modules/home/spector/programs/nixcord.nix
@@ -18,16 +18,6 @@ in
   config = mkIf cfg.enable {
     programs.nixcord = {
       enable = true;
-      config = {
-        themeLinks = [
-          "https://catppuccin.github.io/discord/dist/catppuccin-macchiato.theme.css"
-        ];
-        plugins = {
-          fakeNitro.enable = true;
-          MutualGroupDMs.enable = true;
-          volumeBooster.enable = true;
-        };
-      };
 
       discord = {
         enable = false;
@@ -37,18 +27,57 @@ in
       vesktop = {
         enable = true;
         settings = {
-          minimizeToTray = "on";
-          discordBranch = "stable";
+          # discordBranch = "stable";
           arRPC = "on";
-          splashColor = "#${config.lib.stylix.colors.base05}";
-          splashBackground = "#${config.lib.stylix.colors.base00}";
-          splashTheming = true;
-          checkUpdates = false;
-          disableMinSize = true;
+          # splashColor = "#${config.lib.stylix.colors.base05}";
+          # splashBackground = "#${config.lib.stylix.colors.base00}";
+          # splashTheming = true;
+          # checkUpdates = false;
+          # disableMinSize = true;
           tray = true;
           hardwareAcceleration = true;
           firstLaunch = false;
         };
+      };
+
+      config = {
+        plugins = {
+          # === UI / UX ===
+          alwaysTrust.enable = true;
+          betterFolders.enable = true;
+          betterRoleContext.enable = true;
+          crashHandler.enable = true;
+          experiments.enable = true;
+          fakeNitro.enable = true;
+          fixSpotifyEmbeds.enable = true;
+          imageZoom.enable = true;
+          memberCount.enable = true;
+          permissionsViewer.enable = true;
+          PinDMs.enable = true;
+          quickMention.enable = true;
+          readAllNotificationsButton.enable = true;
+          revealAllSpoilers.enable = true;
+          serverListIndicators.enable = true;
+          showHiddenChannels.enable = true;
+          spotifyControls.enable = true;
+          themeAttributes.enable = true;
+          typingIndicator.enable = true;
+          voiceMessages.enable = true;
+          volumeBooster.enable = true;
+          webContextMenus.enable = true;
+          whoReacted.enable = true;
+
+          # === Privacy ===
+          anonymiseFileNames.enable = true;
+          ClearURLs.enable = true;
+
+          # === Logging / Notifications ===
+          messageLogger.enable = true;
+          relationshipNotifier.enable = true;
+        };
+
+        useQuickCss = true;
+        frameless = true;
       };
     };
   };
