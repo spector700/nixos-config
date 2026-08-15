f981a76b0d29c3216b8888418e7b2f3ae85253ba
spector700 <72362399+spector700@users.noreply.github.com>
2026-08-15 12:46:27 -0500

    fix: add max cores to nix build

--- modules/nixos/os/nix.nix
+++ modules/nixos/os/nix.nix
@@ -48,6 +48,7 @@
 
       # let the system decide the number of max jobs
       max-jobs = "auto";
+      cores = 4;
 
       # continue building derivations even if one fails
       keep-going = true;
